fa23051a3ec37370ccf64c92541f2ff897fcc4a4
Nicolas25vlad <nicolas25vlad@proton.me>
2026-09-24 17:25:38 -0300

    refactor(identity): centralize supported user types

--- app/core/identity.py
+++ app/core/identity.py
@@ -1,5 +1,14 @@
 from typing import Literal, get_args
 
-UserType = Literal["farm_owner", "company_employee", "admin"]
-VALID_USER_TYPES = frozenset(get_args(UserType))
-USER_TYPE_ERROR = "user_type deve ser farm_owner, company_employee ou admin"
+FARM_OWNER_USER_TYPE = "farm_owner"
+COMPANY_EMPLOYEE_USER_TYPE = "company_employee"
+ADMIN_USER_TYPE = "admin"
+
+UserType = Literal[
+    "farm_owner",
+    "company_employee",
+    "admin",
+]
+USER_TYPES = get_args(UserType)
+VALID_USER_TYPES = frozenset(USER_TYPES)
+USER_TYPE_ERROR = "user_type deve ser " + ", ".join(USER_TYPES[:-1]) + " ou " + USER_TYPES[-1]
